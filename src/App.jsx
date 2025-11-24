68c3795712e6bba41fd03866450db513776635e2
ElCapidev <julicgavez1@gmail.com>
2025-11-24 13:47:24 +0100

    fixing update

--- src/App.jsx
+++ src/App.jsx
@@ -41,6 +41,7 @@ const MENU_ITEMS = [
   { id: 8, name: 'Entrecot', price: 18.00, category: 'Principal' },
   { id: 9, name: 'Tarta de Queso', price: 5.50, category: 'Postres' },
   { id: 10, name: 'Café', price: 1.50, category: 'Bebidas' },
+  { id: 11, name: 'Cdafé', price: 11.50, category: 'Bedbidas' },
 ];
 
 const ORDER_STATUS = {
